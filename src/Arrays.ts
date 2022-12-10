89401f444e6d1158775f72ba6cf9a1b04189fd2e
Datodia <dato.diasamidze.02@gmail.com>
2022-12-10 15:31:23 +0400

    feat: add responsive for tablet

---
+++ src/Arrays.ts
@@ -0,0 +1,14 @@
+export const numbers: { num: number, name: string }[] = [
+    {
+        num: 1,
+        name: 'OVERVIEW'
+    },
+    {
+        num: 2,
+        name: 'STRUCTURE'
+    },
+    {
+        num: 3,
+        name: 'SURFACE'
+    }
+]
